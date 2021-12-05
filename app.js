44846aa1068e529c92358bdc98e7c4d09ffd06c8
natthan <natthandosantos@gmail.com>
2021-12-05 14:53:02 -0300

    carrosel 1/2

--- app.js
+++ app.js
@@ -1,36 +1,26 @@
 const carouselSlide = document.querySelector(".slider");
 const carouselImg = document.querySelectorAll(".slider img");
+const slides = document.querySelectorAll(".slide");
+const numberOfSlides = slides.length;
+var slideNumber = 0;
 
-const prevBtn = document.querySelector("#prevBtn");
 const nextBtn = document.querySelector("#nextBtn");
+const prevBtn = document.querySelector("#prevBtn");
 
-let contador = 1;
-const size = carouselImg[0].clientWidth;
+nextBtn.addEventListener("click", function(){
+    slides.forEach( function (slide) {
+        slide.classList.remove ("active")
+        
+    });
+    
+    slideNumber++
 
-carouselSlide.style.transform = "translateX(" + (-size * contador) + "px)";
-
-prevBtn.addEventListener("click", function() {
-    if (contador <= 0) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    contador--;
-    carouselSlide.style.transform = "translateX(" + (-size * contador) + "px)";
-});
-nextBtn.addEventListener("click", function() {
-    if (contador >= carouselImg.lenght -1) return;
-    carouselSlide.style.transition = "transform 0.4s ease-in-out";
-    contador++;
-    carouselSlide.style.transform = "translateX(" + (-size * contador) + "px)";
-});
-
-carouselSlide.addEventListener("transitionend", function() {
-    if (carouselImg[contador].id === "lastClone") {
-        carouselSlide.style.transition = "none";
-        contador = carouselImg.lenght -2;
-        carouselSlide.style.transform = "translateX(" + (-size * contador) + "px)";
+    if(slideNumber > (numberOfSlides - 1)){
+        slideNumber = 0;
     }
-    if (carouselImg[contador].id === "firstClone") {
-        carouselSlide.style.transition = "none";
-        contador = carouselImg.lenght - contador;
-        carouselSlide.style.transform = "translateX(" + (-size * contador) + "px)";
-    }
+
+    slides[slideNumber].classList.add("active");
+    
 });
+
+
